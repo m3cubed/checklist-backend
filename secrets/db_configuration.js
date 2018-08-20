947298d535ffd58018b619a9a50e58028a287d1e
Mike <mike.ma0426@gmail.com>
2018-08-20 18:40:37 -0400

    'commit'

--- secrets/db_configuration.js
+++ secrets/db_configuration.js
@@ -1,9 +1,9 @@
 let user, host, databse, password;
 
 if (process.env.MODE === "production") {
-	user = "fdwcftonyupphr";
-	host = "ec2-54-163-246-5.compute-1.amazonaws.com";
-	database = "d2rp4m1o42lpn";
+	user = "jclpbspioolsgg";
+	host = "ec2-54-235-94-36.compute-1.amazonaws.com";
+	database = "dcpbkj58pc5mfi";
 	password = process.env.DB_PASSWORD;
 } else {
 	user = "node_user";
